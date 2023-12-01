8b4f9d93b3620d460cfe8c42f2e15ea2a44a8899
markonius <markonius@pm.me>
2023-12-01 14:38:46 +0100

    Problem 1: Trebuchet

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+
+fun main() {
+    val problemNumber = arrayOf(1, 1)
+
+    val problems = arrayOf(
+        trebuchet
+    )
+
+    val problem = problems[problemNumber[0] - 1][problemNumber[1]]
+    val output = problem()
+    println(output)
+}
