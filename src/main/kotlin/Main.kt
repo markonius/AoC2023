3039283f659d48b6f88f9d3bfcc7d89137a34301
markonius <markonius@pm.me>
2023-12-04 10:54:34 +0100

    Problem 4: Scratchcards

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,12 @@
 fun main() {
-   val problemNumber = arrayOf(3, 1)
+   val problemNumber = arrayOf(4, 1)
 
    val problems =
          arrayOf(
                trebuchet,
                cubeConundrum,
                gearRatios,
+               scratchcards,
          )
 
    val problem = problems[problemNumber[0] - 1][problemNumber[1]]
