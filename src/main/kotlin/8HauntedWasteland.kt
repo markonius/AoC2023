2c91ddd26b17ebaddb378bd252221fbb0e298418
Marko <markonius@protonmail.ch>
2023-12-09 12:33:55 +0100

    Optimal HauntedWasteland

--- src/main/kotlin/8HauntedWasteland.kt
+++ src/main/kotlin/8HauntedWasteland.kt
@@ -1,119 +1,140 @@
+import kotlin.io.println
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 
-val hauntedWasteland = arrayOf(
-    ::hauntedWasteland0,
-    ::hauntedWasteland1,
-)
+val hauntedWasteland =
+      arrayOf(
+            ::hauntedWasteland0,
+            ::hauntedWasteland1,
+      )
 
 fun hauntedWasteland0(): String {
-    val lines = input.lines()
-    val instructions = lines[0].map { c -> if (c == 'L') 0 else 1 }.toIntArray()
+   val lines = input.lines()
+   val instructions = lines[0].map { c -> if (c == 'L') 0 else 1 }.toIntArray()
 
-    val mapParser = "(\\w+) = \\((\\w+), (\\w+)\\)".toRegex()
-    val map = lines
-        .drop(2)
-        .associate { l ->
+   val mapParser = "(\\w+) = \\((\\w+), (\\w+)\\)".toRegex()
+   val map =
+         lines.drop(2).associate { l ->
             val match = mapParser.find(l)
             val key = match!!.groupValues[1]
             val value = arrayOf(match.groupValues[2], match.groupValues[3])
             key to value
-        }
+         }
 
-    var target = "AAA"
-    var instructionIndex = 0
-    var stepCounter = 0
-    while (true) {
-        stepCounter++
-        val options = map[target]!!
-        target = options[instructions[instructionIndex]]
-        if (target == "ZZZ") {
-            return stepCounter.toString()
-        }
-        instructionIndex = (instructionIndex + 1) % instructions.size
-    }
+   var target = "AAA"
+   var instructionIndex = 0
+   var stepCounter = 0
+   while (true) {
+      stepCounter++
+      val options = map[target]!!
+      target = options[instructions[instructionIndex]]
+      if (target == "ZZZ") {
+         return stepCounter.toString()
+      }
+      instructionIndex = (instructionIndex + 1) % instructions.size
+   }
 }
 
 fun hauntedWasteland1(): String {
-    val lines = input.lines()
-    val instructions = lines[0].map { c -> if (c == 'L') 0 else 1 }.toIntArray()
+   val lines = input.lines()
+   val instructions = lines[0].map { c -> if (c == 'L') 0 else 1 }.toIntArray()
 
-    val mapParser = "(\\w+) = \\((\\w+), (\\w+)\\)".toRegex()
-    val map = lines
-        .drop(2)
-        .associate { l ->
+   val mapParser = "(\\w+) = \\((\\w+), (\\w+)\\)".toRegex()
+   val map =
+         lines.drop(2).associate { l ->
             val match = mapParser.find(l)
             val key = match!!.groupValues[1]
             val value = arrayOf(match.groupValues[2], match.groupValues[3])
             key to value
-        }
+         }
 
-    class Path(val potentialEnds: List<Long>, val period: Long)
+   data class Loop(
+         val stepOffset: Long,
+         val startingNode: String,
+         val firstInstructionIndex: Int,
+         val period: Long
+   )
 
-    class LoopInfo(
-        val stepOffset: Long,
-        val startingNode:
-        String, val firstInstructionIndex: Int,
-        val period: Long
-    )
+   data class Path(val loopInfo: Loop, val potentialEnds: List<Long>)
 
-    fun calculatePath(startingNode: String): Path {
-        fun findLoopStart(): LoopInfo {
-            val visitCache = map.keys.associateWith { _ -> HashMap<Int, Long>() }
+   fun calculatePath(startingNode: String): Path {
+      fun findLoop(): Loop {
+         val visitCache = map.keys.associateWith { _ -> HashMap<Int, Long>() }
 
-            var target = startingNode
-            var instructionIndex = 0
-            var stepCounter = 0L
-            while (true) {
-                stepCounter++
-                val cachedVisit = visitCache[target]!!
-                val visitedAtStep = cachedVisit[instructionIndex]
-                if (visitedAtStep != null) {
-                    println("Found loop: $startingNode")
-                    return LoopInfo(stepCounter - 1, target, instructionIndex, )
-                }
-
-                val options = map[target]!!
-                target = options[instructions[instructionIndex]]
-                instructionIndex = (instructionIndex + 1) % instructions.size
+         var target = startingNode
+         var instructionIndex = 0
+         var stepCounter = 0L
+         while (true) {
+            val cachedVisit = visitCache[target]!!
+            val visitedAtStep = cachedVisit[instructionIndex]
+            if (visitedAtStep != null) {
+               println("Found loop: $startingNode, $target")
+               return Loop(
+                     visitedAtStep,
+                     target,
+                     instructionIndex,
+                     stepCounter - visitedAtStep,
+               )
+            } else {
+               cachedVisit[instructionIndex] = stepCounter
             }
-        }
 
-        fun findPotentialEnds(startingNode: String, firstInstructionIndex: Int): ArrayList<Long> {
-            val potentialEnds = ArrayList<Long>()
-            var target = startingNode
-            var instructionIndex = firstInstructionIndex
-            var stepCounter = 0L
-            while (true) {
-                stepCounter++
-                val options = map[target]!!
-                target = options[instructions[instructionIndex]]
-                if (target[2] == 'Z') {
-                    potentialEnds.add(stepCounter)
-                }
-                instructionIndex = (instructionIndex + 1) % instructions.size
-                if (target == startingNode && instructionIndex == firstInstructionIndex) {
-                    return potentialEnds
-                }
+            val options = map[target]!!
+            target = options[instructions[instructionIndex]]
+            instructionIndex = (instructionIndex + 1) % instructions.size
+            stepCounter++
+         }
+      }
+
+      fun findPotentialEnds(
+            startingNode: String,
+            firstInstructionIndex: Int,
+            stepOffset: Long
+      ): ArrayList<Long> {
+         val potentialEnds = ArrayList<Long>()
+         var target = startingNode
+         var instructionIndex = firstInstructionIndex
+         var stepCounter = stepOffset
+         while (true) {
+            val options = map[target]!!
+            if (target[2] == 'Z') {
+               potentialEnds.add(stepCounter)
+               println("Potential end for $startingNode: $target")
             }
-        }
+            target = options[instructions[instructionIndex]]
+            instructionIndex = (instructionIndex + 1) % instructions.size
+            stepCounter++
+            if (target == startingNode && instructionIndex == firstInstructionIndex) {
+               return potentialEnds
+            }
+         }
+      }
 
-        val loopStart = findLoopStart()
-        val potentialEnds = findPotentialEnds(loopStart.startingNode, loopStart.firstInstructionIndex)
-    }
+      val loop = findLoop()
+      val potentialEnds =
+            findPotentialEnds(loop.startingNode, loop.firstInstructionIndex, loop.stepOffset)
+      return Path(loop, potentialEnds)
+   }
 
-    val starts = map.keys.filter { k -> k[2] == 'A' }
-    val paths = runBlocking(Dispatchers.Default) {
-        starts.map { s ->
-            calculatePath(s)
-        }
-    }
+   val starts = map.keys.filter { k -> k[2] == 'A' }
+   val paths = runBlocking(Dispatchers.Default) { starts.map { s -> calculatePath(s) } }
 
-    return "Done"
+   for (path in paths) {
+      println(path)
+   }
+
+   // There are hidden invariants in the data:
+   // 1. All paths loop.
+   // 2. There are nodes that are traversed in the beginning that are not part of the loop.
+   // 2. All paths only ever reach one potential end.
+   // 3. All ends are reached at steps equal to the period of the loop.
+   //		In other words, the number of steps made before the loop starts can be ignored.
+   val lcm = findLeastCommonMultiple(paths.map { p -> p.potentialEnds[0] })
+   return lcm.toString()
 }
 
 private const val input =
-    """LRRLRRRLRRRLLRRLRRLRLRLRRLLRRLRRLRRRLLLRRRLRRRLRRRLLRRRLRRLLRRLRRLRLRRRLRRLRLRRLRRRLLRRLLRLRRRLLRRLRRLLLRLRRRLRLRLRLLRRRLRLLRRRLRLRRRLRRRLLRRLRRRLLRRLRLLRLRRLLLRRLRRLLLRLLRLRRRLRLRLRRRLRRLLRRRLRLRLRRLRRRLRLRRLRRLRRRLRRRLRRRLRRRLRRLLRRLRLLRRLLRRRLRLLRLRLRRLRRLRLRLRRRLRLRLRRLRLRRLRRRR
+      """LRRLRRRLRRRLLRRLRRLRLRLRRLLRRLRRLRRRLLLRRRLRRRLRRRLLRRRLRRLLRRLRRLRLRRRLRRLRLRRLRRRLLRRLLRLRRRLLRRLRRLLLRLRRRLRLRLRLLRRRLRLLRRRLRLRRRLRRRLLRRLRRRLLRRLRLLRLRRLLLRRLRRLLLRLLRLRRRLRLRLRRRLRRLLRRRLRLRLRRLRRRLRLRRLRRLRRRLRRRLRRRLRRRLRRLLRRLRLLRRLLRRRLRLLRLRLRRLRRLRLRLRRRLRLRLRRLRLRRLRRRR
 
 FPF = (PTN, MPT)
 DGM = (KXM, PRM)
--- src/main/kotlin/Util.kt
+++ src/main/kotlin/Util.kt
@@ -1,5 +1,32 @@
 val allNumbersParser = "\\d+".toRegex()
 
 fun getAllNumbers(s: String): List<Long> {
-    return allNumbersParser.findAll(s).map { m -> m.value.toLong() }.toList()
+   return allNumbersParser.findAll(s).map { m -> m.value.toLong() }.toList()
+}
+
+fun getPrimes(upTo: Long): List<Long> {
+   return (2..upTo).filter { n -> (2 ..< n).none { d -> n % d == 0L } }
+}
+
+fun findLeastCommonMultiple(numbers: List<Long>): Long {
+   val primes = getPrimes(numbers.max())
+   val factors = ArrayList<Long>()
+   var currentStep = numbers.toMutableList()
+
+   while (!currentStep.all { x -> x == 1L }) {
+      primal@ for (prime in primes) {
+         var divisible = false
+         for (i in 0 ..< currentStep.size) {
+            if (currentStep[i] % prime == 0L) {
+               divisible = true
+               currentStep[i] /= prime
+            }
+         }
+         if (divisible) {
+            factors.add(prime)
+            break@primal
+         }
+      }
+   }
+   return factors.reduce { a, f -> a * f }
 }
